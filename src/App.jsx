3166fca248943310e5a76439f7b84e119cd52187
Srivastavayash <srivastavayash017@gmail.com>
2023-08-06 23:42:06 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import styles from './Constants/Style'
+import {Navbar} from './Componenets/Index'
 const App=()=> 
 (
     <div>
-
+     <Navbar/>
     </div>
 );
 export default App
